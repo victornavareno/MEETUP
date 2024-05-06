ea305db1f9290413a0e5d87fa0610f5b9d5e206b
victornavareno <vnavareo@alumnos.unex.es>
2024-05-06 11:18:46 +0200

    popup animation js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const showPopupBtn = document.querySelector(".btn-login");
+
+showPopupBtn.addEventListener("click", () => {
+  // CHANGE THE CLASS NAME WHEN THE LOGIN BUTTON IS CLICKED
+  document.body.classList.toggle("show-popup");
+
+
+});
