25c5ad74f369abdf212fca78e03bc0b32143a793
victornavareno <vnavareo@alumnos.unex.es>
2024-05-06 12:12:28 +0200

    js changes and merging

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
 const showPopupBtn = document.querySelector(".btn-login");
-
+const hidePopupBtn = document.querySelector(".form-popup .close-btn");
+const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
 showPopupBtn.addEventListener("click", () => {
   // CHANGE THE CLASS NAME WHEN THE LOGIN BUTTON IS CLICKED
   document.body.classList.toggle("show-popup");
-
-
 });
+
+
+hidePopupBtn.addEventListener("click", () => {
+  // CHANGE THE CLASS NAME WHEN THE CLOSE BUTTON IS CLICKED
+  document.body.classList.remove("show-popup");
+});
+
+
+loginSignupLink.forEach(link=>{
+    link.addEventListener("click")
+})
