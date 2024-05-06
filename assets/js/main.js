5e0cdcebfefc5f7db799dd96731ddb3a72025add
victornavareno <vnavareo@alumnos.unex.es>
2024-05-06 13:41:39 +0200

    LOGIN SIGNUP FINISHED

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const showPopupBtn = document.querySelector(".btn-login");
 const hidePopupBtn = document.querySelector(".form-popup .close-btn");
 const loginSignupLink = document.querySelectorAll(".form-box .bottom-link a");
+const formPopup = document.querySelector(".form-popup");
+
+
 showPopupBtn.addEventListener("click", () => {
   // CHANGE THE CLASS NAME WHEN THE LOGIN BUTTON IS CLICKED
   document.body.classList.toggle("show-popup");
@@ -14,5 +17,9 @@ hidePopupBtn.addEventListener("click", () => {
 
 
 loginSignupLink.forEach(link=>{
-    link.addEventListener("click")
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        // CHANGE THE CLASS NAME WHEN THE SIGNUP LINK IS CLICKED
+        formPopup.classList[link.id === "signup-link" ? 'add' : 'remove']("show-signup");
+    })
 })
